39adaee67b8417825539e22f5a685253849dbb0a
SethupathyD <sethupathy.d@syncfusion.com>
2024-07-18 18:51:31 +0530

    Code changes

---
+++ MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace SimpleSample
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+
+}
---
+++ NewContent1.xaml.cs
@@ -0,0 +1,9 @@
+namespace SimpleSample;
+
+public partial class NewContent1 : ContentView
+{
+	public NewContent1()
+	{
+		InitializeComponent();
+	}
+}
